48cc7998f9992c18698ca300fb136eaa31b84280
tolossamuel <tolossamuel1@gmail.com>
2024-02-11 10:41:41 +0300

    codealpha task1

---
+++ FIBONACCI GENERATOR.py
@@ -0,0 +1,15 @@
+def fibonacci_recursive(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = fibonacci_recursive(n - 1)
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
+
+# Example: Generate the first 10 Fibonacci numbers
+result_recursive = fibonacci_recursive(10)
+print(result_recursive)
